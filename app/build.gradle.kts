e325994dc7de95ae1c3cd5d86e006d457719fabf
UmerCodez <umerfarooq.phone@gmail.com>
2025-12-07 20:05:27 +0500

    build: Bump version to 1.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.github.umer0586.sensagram"
         minSdk = 21
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.5.0"
+        versionCode = 9
+        versionName = "1.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
